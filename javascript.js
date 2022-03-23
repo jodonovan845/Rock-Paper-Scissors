ba5449e1469a78078241453e96cc36a93a779fb2
John <jodonovan845@gmail.com>
2022-03-23 18:46:27 -0400

    completed computerPlay function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
 console.log("test of javascript file");
+
+function computerPlay () {
+    let random = Math.floor(Math.random() * 3) + 1;
+    if (random === 1) {
+        return "rock";
+    } else if (random === 2) {
+        return "scissors";
+    } else {
+        return "paper";
+    }
+}
+
+console.log(computerPlay());
