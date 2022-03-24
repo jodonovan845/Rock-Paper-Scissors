f83114790e7e6e71b197b43ec4dc03bfdb31bcc0
John <jodonovan845@gmail.com>
2022-03-23 21:03:58 -0400

    started work on game function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 console.log("test of javascript file");
 
 function computerPlay () {
+    //this function randomly picks RPS for computer
     let random = Math.floor(Math.random() * 3) + 1;
     if (random === 1) {
         return "rock";
@@ -11,4 +12,14 @@ function computerPlay () {
     }
 }
 
-console.log(computerPlay());
+//this takes user's choice and assigns it to variable playerSelection
+let playerSelection = prompt("Please choose rock, paper, or scissor.");
+
+if(playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+    
+}
+
+
+
+//console.log(computerPlay());
+//console.log(playerSelection);
